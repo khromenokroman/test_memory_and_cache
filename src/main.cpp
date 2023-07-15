038fea95d987c4d208ee73d7931e29658a32f3eb
rkhromenok <rkhromenok@t-argos.ru>
2023-07-15 21:01:06 +0200

    добавил обработку осключений

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
-#include "timer.hpp"
 #include "matrix.hpp"
+#include "timer.hpp"
+#include <exception>
 
 int main() {
 
-  Matrix array;
+  try {
+    Matrix array;
 
-  // array.show_array();
-  // array.sum_array_in_order();
-  array.sum_array_in_columns();
-
+    // array.show_array();
+    // array.sum_array_in_order();
+    array.sum_array_in_columns();
+  } catch (std::exception const &ex) {
+    printf("Ошибка при работе с классом!\n");
+  } catch (...) {
+    printf("Неожиданная ошибка!\n");
+  }
 }
